4c427fb1dd6a1e18cc22898730b4fce4acc9346f
Alex Nakagawa <alexnakagawa3@gmail.com>
2017-12-22 21:19:06 -0800

    classroom_eda added

---
+++ ml/classroom_eda.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+
+# Define the lambda function: categorize_label
+categorize_label = lambda x: x.astype('category')
+
+# Convert df[LABELS] to a categorical type
+df[LABELS] = df[LABELS].apply(categorize_label, axis=0)
+
+# Print the converted dtypes
+print(df[LABELS].dtypes)
+
+
+
+
+
+# Calculate number of unique values for each label: num_unique_labels
+num_unique_labels = df[LABELS].apply(pd.Series.nunique)
+
+# Plot number of unique values for each label
+num_unique_labels.plot(kind='bar')
+
+# Label the axes
+plt.xlabel('Labels')
+plt.ylabel('Number of unique values')
+
+# Display the plot
+plt.show()
+
+
+
+
+
+
+# Compute and print log loss for 1st case
+correct_confident = compute_log_loss(correct_confident, actual_labels)
+print("Log loss, correct and confident: {}".format(correct_confident)) 
+
+# Compute log loss for 2nd case
+correct_not_confident = compute_log_loss(correct_not_confident, actual_labels)
+print("Log loss, correct and not confident: {}".format(correct_not_confident)) 
+
+# Compute and print log loss for 3rd case
+wrong_not_confident = compute_log_loss(wrong_not_confident, actual_labels)
+print("Log loss, wrong and not confident: {}".format(wrong_not_confident)) 
+
+# Compute and print log loss for 4th case
+wrong_confident = compute_log_loss(wrong_confident, actual_labels)
+print("Log loss, wrong and confident: {}".format(wrong_confident)) 
+
+# Compute and print log loss for actual labels
+actual_labels = compute_log_loss(actual_labels, actual_labels)
+print("Log loss, actual labels: {}".format(actual_labels))
+
+
+
+
+
+
+
+# Model
