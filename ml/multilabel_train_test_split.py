11346e846b43ceaf9e7cf2e8e5eb7409a65f6536
Alex Nakagawa <alexnakagawa3@gmail.com>
2017-12-22 23:43:08 -0800

    multilabel_train_test_split.py

---
+++ ml/multilabel_train_test_split.py
@@ -0,0 +1,37 @@
+'''
+This is an example of a dendrogram plot showing the hierarchical structure of clustering.
+
+Inspired from the "Unsupervised Learning" course on Datacamp.com
+Author: Alex Nakagawa
+'''
+
+import numpy as np
+import pandas as pd
+
+def multilabel_train_test_split(X, Y, size, min_count=5, seed=None):
+    """ Takes a features matrix `X` and a label matrix `Y` and
+        returns (X_train, X_test, Y_train, Y_test) where all
+        classes in Y are represented at least `min_count` times.
+    """
+    index = Y.index if isinstance(Y, pd.DataFrame) else np.arange(Y.shape[0])
+
+    test_set_idxs = multilabel_sample(Y, size=size, min_count=min_count, seed=seed)
+    train_set_idxs = np.setdiff1d(index, test_set_idxs)
+
+    test_set_mask = index.isin(test_set_idxs)
+    train_set_mask = ~test_set_mask
+
+    return (X[train_set_mask], X[test_set_mask], Y[train_set_mask], Y[test_set_mask])
+
+
+# Create the new DataFrame: numeric_data_only
+numeric_data_only = df[NUMERIC_COLUMNS].fillna(-1000)
+
+# Get labels and convert to dummy variables: label_dummies
+label_dummies = pd.get_dummies(df[LABELS])
+
+X_train, X_test, y_train, y_test = multilabel_train_test_split(numeric_data_only,
+                                                               label_dummies,
+                                                               size=0.2, 
+                                                               seed=123)
+
