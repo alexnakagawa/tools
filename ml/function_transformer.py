d921780c15aa49b29ba8b326fe3d088629509ed7
Alex Nakagawa <alexnakagawa3@gmail.com>
2017-12-24 18:20:35 -0800

    function_transformer added

---
+++ ml/function_transformer.py
@@ -0,0 +1,29 @@
+'''
+This is an example of creating a FunctionTransformer that is friendly to scikit learn's
+fit() and transform() methods.
+
+Inspired from the "Machine Learning with the Experts: School Budgets" course on Datacamp.com
+Author: Alex Nakagawa
+'''
+
+
+# Import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer
+
+# Obtain the text data: get_text_data
+get_text_data = FunctionTransformer(lambda x: x['text'], validate=False)
+
+# Obtain the numeric data: get_numeric_data
+get_numeric_data = FunctionTransformer(lambda x: x[['numeric', 'with_missing']], validate=False)
+
+# Fit and transform the text data: just_text_data
+just_text_data = get_text_data.fit_transform(sample_df)
+
+# Fit and transform the numeric data: just_numeric_data
+just_numeric_data = get_numeric_data.fit_transform(sample_df)
+
+# Print head to check results
+print('Text Data')
+print(just_text_data.head())
+print('\nNumeric Data')
+print(just_numeric_data.head())
