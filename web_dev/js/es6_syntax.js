521c12304471cbe250745ddc9e0e23c2c1350ec5
Alex Nakagawa <alexnakagawa3@gmail.com>
2018-01-12 23:14:38 -0800

    rename to es6_syntax and added javascript embedding

--- web_dev/js/es6_syntax.js
+++ web_dev/js/es6_syntax.js
@@ -1,8 +1,11 @@
-// You can also concatenate different arrays of objects together using a ... notation.
 // This script was inspired by the freeCodeCamp Javascript workshop: www.youtube.com/watch?v=5MPHo5jGXwQ
 
 // Author: Alex Nakagawa
 
+
+
+// 1. Concatenation of Arrays
+// You can also concatenate different arrays of objects together using a ... notation.
 let names =         ['Alex', 'Bob', 'Carl'];
 let moreNames =     ['Danielle', 'Edna'];
 let evenMoreNames = ['Forrest'];
@@ -11,5 +14,8 @@ let allNames = [...names,
 				...moreNames,
 				...evenMoreNames
 ];
-
 console.log(allNames)
+
+// 2. Embed Javascript directly inline with strings
+const s = `Hi my name is ${names[0]}`
+console.log(s)
