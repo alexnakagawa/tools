2433f1ad3cc7613cff532c76f5954f60d650172a
Alex Nakagawa <alexnakagawa3@gmail.com>
2018-02-06 01:02:23 -0800

    structure added

---
+++ c++/structure.cpp
@@ -0,0 +1,28 @@
+/* 
+In many ways, the c++ language is the same as Java, so this should not be difficult to digest.
+Author: Alex Nakagawa
+*/
+
+// This is called a preprocessor directive. <iostream> is part of the "C++ Standard Library"
+// It is used for writing to the screen and getting input from the user
+#include <iostream>
+ 
+ // This is function declaration. Every program must have a main function.
+int main()
+{
+	// << is the output operator
+	// std::cout represents the console/screen
+	// Since this is a statement, we end it with a semicolon (;)
+	std::cout << "Hello world!";
+	return 0;
+}
+
+
+
+TODO:
+// typedef
+
+// the arguments of main
+
+// The difference between & and *
+
